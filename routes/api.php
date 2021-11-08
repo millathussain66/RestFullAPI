0040b0a8ac296b6387a116b8f8d7ddf2c4e9b0dc
MD.Millat Hussain <89291761+millat127166@users.noreply.github.com>
2021-11-08 11:17:26 +0000

    Add api Resorch Route Add new fild
    Add api Resorch Route Add new fild

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Country\CountryController;
+use App\Http\Controllers\CountryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,12 +9,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('country', [CountryController::class,'country']);
-// Get one data use id
-Route::get('country/{id}', [CountryController::class,'countryone']);
 
-// Add Data in table
+
+Route::apiResource('country', CountryController::class);
 
 
 
-Route::post('countryCreate', [CountryController::class,'countryCreate']);
