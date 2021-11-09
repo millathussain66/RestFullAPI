42a6203d3897ca1105f41f27eca73680fa16b22c
MD.Millat Hussain <89291761+millat127166@users.noreply.github.com>
2021-11-09 13:11:19 +0000

    Basic Auth added

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,8 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\Authkey::class
+            // \App\Http\Middleware\Authkey::class
+            \App\Http\Middleware\Autbasic::class
         ],
     ];
 
